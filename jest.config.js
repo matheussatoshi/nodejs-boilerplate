86c460170741dc0a694120ed9cb40f7b2d728525
matheussatoshi1 <matheussdev3@gmail.com>
2025-02-15 05:21:13 -0400

    chore(config):  add 'preset' and 'watch' options to Jest config

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,12 @@ const config = {
   transform: {
     ".+\\.(ts|tsx)$": "ts-jest",
   },
+  preset: "ts-jest",
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
     "<rootDir>/tests/e2e/cypress",
   ],
+  watch: false,
   moduleNameMapper: {
     "@/tests/(.*)": "<rootDir>/tests/$1",
     "@/(.*)": "<rootDir>/src/$1",
